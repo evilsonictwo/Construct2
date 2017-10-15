ba5d072a03bfa00c452658d20de89dc62bd5a743
DESKTOP-TTF2DBR\jeferson <evilsonictwo@gmail.com>
2017-10-15 21:41:46 -0200

    [ENH] Criação da primeira cena, inimigos e outras melhorias.

---
+++ Sprites/Scenes/Forest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tile" tilewidth="64" tileheight="64" tilecount="108" columns="9">
+ <image source="../Tiled e backgrounds/forest/ai/Tile.png" trans="ff00ff" width="600" height="800"/>
+</tileset>
